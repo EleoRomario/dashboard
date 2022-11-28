3704926905aef754e7786184d181613afd2c35f1
EleoRomario <eleo.romario@gmail.com>
2022-11-28 14:59:24 -0500

    final commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { Home2 } from 'iconsax-react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a> <Home2/>
         </h1>
 
         <p className={styles.description}>
